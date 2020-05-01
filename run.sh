73fa1bb3ab4ab9832fd3d2cf3d7a7d478a8d0aff
nivedr <nived.rajaraman@gmail.com>
2020-05-01 12:47:22 -0700

    first commit with new bash script

--- run.sh
+++ run.sh
@@ -3,3 +3,10 @@
 jemdoc/jemdoc -c mysite.conf publications.jemdoc
 jemdoc/jemdoc -c mysite.conf index.jemdoc
 jemdoc/jemdoc -c mysite.conf cv.jemdoc
+
+echo 'Enter commit message'
+read msg
+
+git add .
+git commit -m "$msg"
+git push
