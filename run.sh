ae13c1b386e63d092546989760be65a2ac4b8d90
nivedr <nived.rajaraman@gmail.com>
2020-09-02 14:30:15 -0700

    edited url

--- run.sh
+++ run.sh
@@ -10,3 +10,5 @@ read msg
 git add .
 git commit -m "$msg"
 git push
+
+rsync -av /Users/nived.rajaraman/Dropbox/websites/nivedr.github.io/ -e ssh nived.rajaraman@login.eecs.berkeley.edu:/home/eecs/nived.rajaraman/public_html
