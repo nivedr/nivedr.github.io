dd26ac0574337f6fe8e2f8555c2c47715c4e7e6c
nivedr <nived.rajaraman@gmail.com>
2021-09-30 10:47:24 -0700

    add blog

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ jemdoc/jemdoc -c mysite.conf publications.jemdoc
 jemdoc/jemdoc -c mysite.conf index.jemdoc
 jemdoc/jemdoc -c mysite.conf cv.jemdoc
 jemdoc/jemdoc -c mysite.conf ./papers/*.jemdoc
+jemdoc/jemdoc -c mysite.conf blog.jemdoc
 
 echo 'Enter commit message'
 read msg
