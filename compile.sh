f2215ba9855cfcbc37cf494b3b77735ca9180fde
nivedr <nived.rajaraman@gmail.com>
2023-08-28 14:59:19 -0700

    add academicpages initial commit

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-jemdoc/jemdoc -c mysite.conf publications.jemdoc
-jemdoc/jemdoc -c mysite.conf index.jemdoc
-jemdoc/jemdoc -c mysite.conf cv.jemdoc
-jemdoc/jemdoc -c mysite.conf ./papers/*.jemdoc
